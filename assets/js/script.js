3c91a888a25d885b820a9522c5fd5713d795875a
Callyel Alves <callyelalves@hotmail.com>
2021-01-14 19:37:59 -0300

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,28 @@
-let create = document.getElementById('loginUp');
+const login = document.getElementById('loginIn');
+const create = document.getElementById('loginUp');
+const loginInput = document.getElementById('loginInput').value;
+const menError = document.getElementById('loginError');
+
+document.querySelector('#createAccount').addEventListener('click', () => {
+    hiden();
+});
+
+function createAccount(){
+    let username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    let name = localStorage.getItem(username);
+    localStorage.setItem('username', name)
+    alert(name);
+}
+
+const logar = () => {
+    if(loginInput === ""){
+        console.log('certo')
+        menError.style.display = 'block';
+    }
+
+}
 
 function show(){
     create.style.display = 'block';
