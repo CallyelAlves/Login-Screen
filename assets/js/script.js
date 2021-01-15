1930ea13c2f48dfd0290769a478b794345121641
Callyel Alves <callyelalves@hotmail.com>
2021-01-15 16:03:00 -0300

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,43 @@
 const login = document.getElementById('loginIn');
 const create = document.getElementById('loginUp');
-const loginInput = document.getElementById('loginInput').value;
-const menError = document.getElementById('loginError');
-
-document.querySelector('#createAccount').addEventListener('click', () => {
-    hiden();
-});
+const menErrorLogin = document.getElementById('loginError');
+const menErrorCreate = document.getElementById('createError')
 
 function createAccount(){
-    let username = document.getElementById('username').value;
+    const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    let name = localStorage.getItem(username);
-    localStorage.setItem('username', name)
-    alert(name);
+
+    if(username === "" || email === "" || password === ""){
+
+        menErrorCreate.style.display = 'block';
+    }else{
+        
+        localStorage.setItem('name', username);
+        localStorage.setItem('mail', email);
+        localStorage.setItem('pass', password);
+        hiden();
+    }
 }
 
 const logar = () => {
-    if(loginInput === ""){
-        console.log('certo')
-        menError.style.display = 'block';
-    }
+    const loginInputName = document.getElementById('loginInputName').value;
+    const loginInputPassword = document.getElementById('loginInputPassword').value;
 
+    if(loginInputName === localStorage.getItem('name') && loginInputPassword === localStorage.getItem('pass')){
+        menErrorLogin.style.display = 'none';
+        let loading = document.getElementById('loading');
+        loading.style.display = 'flex';
+
+    }else{
+        menErrorLogin.style.display = 'block';
+    }
 }
 
 function show(){
     create.style.display = 'block';
 }
+
 function hiden(){
     create.style.display = 'none';
 }
